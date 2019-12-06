d2b7c3b4564be26004de25f2624312caee0fb2fa
William Hall <billhall168@gmail.com>
2019-12-06 10:50:49 -0700

    arranged files outside app folder

--- app/controllers/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    index: (req, res) => {
-        res.send("app/views/index");
-    }
-
-}
-// app\views\index.handlebars
--- models/Recipe.js
+++ models/Recipe.js
@@ -5,28 +5,28 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
-  // `title` is required and of type String
-  title: {
+var RecipeSchema = new Schema({
+  // `name` is required and of type String
+  name: {
     type: String,
     required: true
   },
+
   // `link` is required and of type String
   link: {
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
+
+  // `image` is required and of type String
+  image: {
+    type: String,
+    required: true
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Recipe = mongoose.model("Recipe", RecipeSchema);
 
 // Export the Article model
-module.exports = Article;
+module.exports = Recipe;
